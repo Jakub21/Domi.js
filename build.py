a4cafbec5f396e3af00b5a9b99f9d083e4c4a4dd
Jakub21 <jakubp2101@gmail.com>
2020-06-10 13:23:15 +0200

    Add builder script

---
+++ build.py
@@ -0,0 +1,30 @@
+from os import walk
+
+def build(name, version, repo, license, dir):
+  header = f'''// {name} v{version}
+  // Github {repo}
+  // {license} License
+  '''
+  source = ''
+  for path, dirs, files in walk(dir):
+    for file in files:
+      source += open(f'{dir}/{file}', 'r', encoding='utf-8').read()
+
+  source = source.replace('\n\n', '\n')
+  clean = ''
+  for line in source.split('\n'):
+    if 'class' in line and clean[-1] != '\n': clean += '\n'
+    if '//' in line: line = line.split('//')[0]
+    clean += line
+  source = header + clean + '\n'
+
+  while '  ' in source: source = source.replace('  ', ' ')
+  open('domi.js', 'w', newline='\n').write(source)
+
+if __name__ == '__main__':
+  name = 'Domi.js'
+  version = '0.1.0'
+  repo = 'https://github.com/Jakub21/Domi.js'
+  license = 'MIT'
+  dir = './source'
+  build(name, version, repo, license, dir)
