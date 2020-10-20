3efafdde17bdcd25c61deb9cc066ebc7ab75dac5
--local <jakubp2101@gmail.com>
2020-10-20 19:36:50 +0200

    Create $empty and add optional ID parameter in $create

--- source/Aliases.js
+++ source/Aliases.js
@@ -1,5 +1,13 @@
 let $id = (id) => { return document.getElementById(id); }
-let $cn = (id) => { return document.getElementsByClassName(id); }
-let $tag = (id) => { return document.getElementsByTagName(id); }
-let $create = (tag) => { return document.createElement(tag); }
+let $cn = (cn) => { return document.getElementsByClassName(cn); }
+let $tag = (tag) => { return document.getElementsByTagName(tag); }
+let $qr = (query) => { return document.querySelector(query); }
+let $create = (tag, id=undefined) => {
+  let elm = document.createElement(tag);
+  if (id != undefined) elm.id = id;
+  return elm;
+}
 let $on = (elm, key, cb) => { elm.addEventListener(key, cb); }
+let $empty = (elm) => {
+  while (elm.lastChild) { elm.removeChild(elm.lastChild); }
+}
