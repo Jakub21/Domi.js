02604d0cb0e35b7c79ace5b2206a0725e0e71947
--local <jakubp2101@gmail.com>
2020-11-05 16:03:45 +0100

    Update AnimatedSwitcher.js

--- source/AnimatedSwitcher.js
+++ source/AnimatedSwitcher.js
@@ -6,25 +6,25 @@ class AnimatedSwitcher extends Switcher {
   }
   goto(id) {
     let current = this.sections[this.current];
+    let next = this.sections[id];
     if (current != undefined) {
       current.section.classList.add(this.anim.leavingCn);
     }
+    next.section.classList.add(this.anim.enteringCn);
     setTimeout((id) => {
-      if (this.current != '')
-        this.sections[this.current].hide();
       let current = this.sections[this.current];
       let next = this.sections[id];
-      next.section.classList.add(this.anim.enteringCn);
+      if (current != undefined) this.sections[this.current].hide();
       next.show();
-      if (current != undefined)
-        current.section.classList.remove(this.anim.leavingCn);
-
-      this.previous = this.current;
-      this.current = id;
     }, this.anim.lDelay, id);
     setTimeout((id) => {
+      let current = this.sections[this.current];
       let next = this.sections[id];
       next.section.classList.remove(this.anim.enteringCn);
+      if (current != undefined)
+        current.section.classList.remove(this.anim.leavingCn);
+      this.previous = this.current;
+      this.current = id;
     }, this.anim.lDelay+this.anim.eDelay, id);
   }
 }
