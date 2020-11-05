485864694c8f0ea4103d421131899963e6321069
--local <jakubp2101@gmail.com>
2020-11-05 15:16:27 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1,26 +1,32 @@
 from os import walk
 
-def build(name, version, repo, license, dir):
+def build(name, version, repo, license, dir, forceBefore=[], forceAfter=[], minify=True):
   header = f'''// {name} v{version}
 // Github {repo}
 // {license} License
 '''
   forceNewline = ['class', 'function', 'var', 'let', 'static']
-  source = ''
+  data = {}
   for path, dirs, files in walk(dir):
     for file in files:
-      source += open(f'{dir}/{file}', 'r', encoding='utf-8').read()
+      id = file.split('.')[0]
+      data[id] = open(f'{dir}/{file}', 'r', encoding='utf-8').read()
+  source = ''
+  for id in forceBefore: source += data[id]
+  for id in data.keys():
+    if id not in forceBefore+forceAfter: source += data[id]
+  for id in forceAfter: source += data[id]
 
-  source = source.replace('\n\n', '\n')
-  clean = ''
-  for line in source.split('\n'):
-    for word in forceNewline:
-      if line.startswith(word) and not clean.endswith('\n'): clean += '\n'
-    if '//' in line: line = line.split('//')[0]
-    clean += line
-  source = header + clean + '\n'
-
-  while '  ' in source: source = source.replace('  ', ' ')
+  if minify:
+    source = source.replace('\n\n', '\n')
+    clean = ''
+    for line in source.split('\n'):
+      for word in forceNewline:
+        if line.startswith(word) and not clean.endswith('\n'): clean += '\n'
+      if '//' in line: line = line.split('//')[0]
+      clean += line
+    source = header + clean + '\n'
+    while '  ' in source: source = source.replace('  ', ' ')
   return source
 
 if __name__ == '__main__':
@@ -29,5 +35,8 @@ if __name__ == '__main__':
   repo = 'https://github.com/Jakub21/Domi.js'
   license = 'MIT'
   dir = './source'
-  source = build(name, version, repo, license, dir)
+  forceBefore = []
+  forceAfter = ['AnimatedSwitcher']
+  minify = True
+  source = build(name, version, repo, license, dir, forceBefore, forceAfter, minify)
   open(name, 'w', newline='\n').write(source)
