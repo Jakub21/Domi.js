f9f57547b2ce304ef463cf74d8d2c2c4a86c0c9d
--local <jakubp2101@gmail.com>
2020-11-29 13:49:42 +0100

    Add optional "from" parameter in element getters

--- source/Aliases.js
+++ source/Aliases.js
@@ -1,8 +1,29 @@
-let $id = (id) => { return document.getElementById(id); }
-let $cn = (cn) => { return document.getElementsByClassName(cn); }
-let $tag = (tag) => { return document.getElementsByTagName(tag); }
-let $qr = (query) => { return document.querySelector(query); }
-let $remove = (elm) => { elm.parentNode.removeChild(elm); };
+
+let $id = (id, from=undefined) => {
+  if (from != undefined)
+    return from.querySelector(`#${id}`);
+  return document.getElementById(id);
+}
+let $cn = (cn, from=undefined) => {
+  if (from != undefined)
+    return from.querySelectorAll(`.${cn}`);
+  return document.getElementsByClassName(cn);
+}
+let $tag = (tag, from=undefined) => {
+  if (from != undefined)
+    return from.querySelectorAll(tag);
+  return document.getElementsByTagName(tag);
+}
+let $qr = (query, from=undefined) => {
+  if (from == undefined)
+    from = document;
+  return from.querySelector(query);
+}
+
+let $remove = (elm) => {
+  elm.parentNode.removeChild(elm);
+};
+
 let $create = (tag, id=undefined) => {
   let elm = document.createElement(tag);
   if (id != undefined) elm.id = id;
@@ -13,7 +34,9 @@ let $text = (text, id=undefined) => {
   if (id != undefined) elm.id = id;
   return elm;
 }
-let $on = (elm, key, cb) => { elm.addEventListener(key, cb); }
+let $on = (elm, key, cb) => {
+  elm.addEventListener(key, cb);
+}
 let $empty = (elm) => {
   while (elm.lastChild) { elm.removeChild(elm.lastChild); }
 }
