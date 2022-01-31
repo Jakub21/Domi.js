5c868e42cc86ac5f5f2a8abcdcfbc5611c407c4b
--local <jakubp2101@gmail.com>
2022-01-31 17:41:00 +0100

    Add version check

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const fs = require('fs');
 
+const currentVersion = JSON.parse(fs.readFileSync(
+  `${__dirname}/package.json`)).version;
+
 const entryPoint = `${__dirname}/webIndex.js`;
 const buildDir = `${__dirname}/build/`;
 const buildFile = `${buildDir}/Domi.js`;
 
 let buildPackage = () => {
+  fs.mkdirSync(buildDir, {recursive: true});
   const browserify = require('browserify')();
   browserify.add(entryPoint);
   let stream = browserify.bundle();
-  let content = '';
+  let content = `// version ${currentVersion}\n`;
   stream.on('data', (data) => { content += data; });
   stream.on('end', () => { fs.writeFileSync(buildFile, content); });
 }
 
+let versionMatch = () => {
+  const bundleVersion = fs.readFileSync(buildFile, 'utf-8').split('\n')[0]
+    .split(' ').splice(-1)[0];
+  return bundleVersion == currentVersion;
+}
+
 fs.stat(buildFile, (err) => {
-  if (err == null) return;
-  fs.mkdirSync(buildDir, {recursive: true});
+  if (err == null) {
+    if (!versionMatch()) return;
+  }
   buildPackage();
 });
 
